0e49182b9860fe9944d600baabd35aac780ada58
JaebeomHan <happycorki0616@gmail.com>
2021-01-09 08:15:34 +0900

    [Ref] 2주차 과제 수정사항 적용

--- Jaebeom/2주차 과제/Q3.py
+++ Jaebeom/2주차 과제/Q3.py
@@ -1,6 +1,45 @@
 
-all_sentence = []
-count = 0
+## 배열 만드는 함수
+def make_list(sentence):
+
+    length = len(sentence)
+    num = 0
+    list_result = []
+    while( length > 0 ):
+        result = [ (sentence[num]) , int(sentence.count(sentence[num])) ] # 각 단어의 개수 배열로 지정
+        list_result.append(result)
+        length -= 1
+        num += 1
+
+    new_list_result = []
+    for w in list_result:
+        if w not in new_list_result:
+            new_list_result.append(w)  # 중복되는 배열 제거
+    return new_list_result
+
+## 출력 담당하는 함수
+def order_print(new_list_result, count):
+
+    new_list_result.sort(key = lambda x:(x[1], x[0]))  # 순서 지정 (숫자로 오름차순)
+    print("[ Sentence %d ]" % count)
+    for i in new_list_result:
+        for j in i:
+            print(j, end=' ')
+        print()
+
+## 출력 담당하는 함수 (END 입력 시)
+def order_print_END(new_list_result):
+
+    new_list_result.sort(key = lambda x:(x[1], x[0]))  # 순서 지정 (숫자로 오름차순)
+    print("[ All Sentence ]")
+    for i in new_list_result:
+        for j in i:
+            print(j, end=' ')
+        print()
+
+
+total_sentence = []
+count_a = 1
 
 while True:
     
@@ -10,56 +49,11 @@ while True:
         break
 
     else: # 정상 진행
-            all_sentence.extend(sentence) # while절이 끝나고 사용될 배열 미리 선언
-            length = len(sentence) # 배열의 길이
-            num = 0
-            list_result = []
-
-            while( length > 0 ):
-                result = [ (sentence[num]), int(sentence.count(sentence[num])) ] # 배열
-                list_result.append(result) # 배열 안의 배열 (2차)
-                length -= 1
-                num += 1
-
-            new_list_result = []
-            for v in list_result:
-                if v not in new_list_result:
-                    new_list_result.append(v)  # 2차 배열 중 중복되는 배열 제거
-                    
-            new_list_result.sort(key = lambda x:(x[1], x[0]))   # 2차 배열을 오름차순으로 정렬
-
-            print("< Sentence %d >" % count)
-            count += 1
-
-            for i in new_list_result:   # 출력...
-                for j in i:  
-                    print(j, end=' ')
-                print()
+        total_sentence += sentence
+        order_print(make_list(sentence), count_a)
+        count_a += 1
 
 # 위에서 했던 과정 똑같이 반복
-
-all_length = len(all_sentence)
-all_num = 0
-all_list_result = []
-
-while( all_length > 0 ):
-    all_result = [ (all_sentence[all_num]), int(all_sentence.count(all_sentence[all_num])) ]
-    all_list_result.append(all_result)
-    all_length -= 1
-    all_num += 1
-
-new_all_list_result = []
-for w in all_list_result:
-    if w not in new_all_list_result:
-        new_all_list_result.append(w)
-
-new_all_list_result.sort(key = lambda x:(x[1], x[0]))
-
-print("< All Sentence >")
-
-for i in new_all_list_result:
-    for j in i:
-        print(j, end=' ')
-    print()
+order_print_END(make_list(total_sentence))
 
 print("프로그램 종료")
